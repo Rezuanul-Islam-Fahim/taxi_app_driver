3a0e0d93d55372a6f04893b7c21b0d11d13b3966
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-08-25 23:12:22 +0600

    Add firebase plugins and initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
