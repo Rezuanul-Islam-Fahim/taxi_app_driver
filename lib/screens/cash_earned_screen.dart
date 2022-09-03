f226df531cc549007584ff591fa517c5e802e940
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-09-04 01:25:28 +0600

    Implement cash earned screen

--- lib/screens/cash_earned_screen.dart
+++ lib/screens/cash_earned_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../models/trip_model.dart';
+import '../services/database_service.dart';
+
 class CashEarnedScreen extends StatelessWidget {
   const CashEarnedScreen({Key? key}) : super(key: key);
 
@@ -11,6 +14,59 @@ class CashEarnedScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cash Earned'),
         backgroundColor: Colors.black,
+      ),
+      body: FutureBuilder(
+        future: DatabaseService().getDriverCompletedTrips(),
+        builder: (BuildContext context, AsyncSnapshot<List<Trip>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          List<Trip> trips = snapshot.data as List<Trip>;
+          double totalEarned = trips.fold<double>(
+            0,
+            (previousValue, element) => previousValue + element.cost!,
+          );
+
+          return trips.isNotEmpty
+              ? Center(
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Total Cash Earned',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Chip(
+                      label: Text(
+                        '\$${totalEarned.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ],
+                ))
+              : const Center(
+                  child: Text('No Completed Trip Found'),
+                );
+        },
       ),
     );
   }
